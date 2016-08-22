b699a02b94393e7d10dd2c80c419267afc84ebcd
Engin Kartal <engin.kartal@yellow.com.tr>
2016-08-22 13:49:11 +0300

    Add Lumen + MongoDB Document Support Fix

---
+++ src/Lumen/DbLogger.php
@@ -0,0 +1,138 @@
+<?php namespace Sofa\Revisionable\Lumen;
+
+use Sofa\Revisionable\Logger;
+use Illuminate\Database\ConnectionInterface;
+use DateTime;
+
+class DbLogger implements Logger
+{
+    /**
+     * Custom database connection.
+     *
+     * @var \Illuminate\Database\ConnectionInterface
+     */
+    protected $connection;
+
+
+
+    /**
+     * Default database connection.
+     *
+     * @var \Illuminate\Database\ConnectionInterface
+     */
+    protected $defaultConnection;
+
+    /**
+     * Revisions table name.
+     *
+     * @var string
+     */
+    protected $table;
+
+    /**
+     * Create a new DbLogger.
+     *
+     * @param \Illuminate\Database\ConnectionInterface $connection
+     * @param string $table
+     */
+    public function __construct(ConnectionInterface $connection, $table)
+    {
+        $this->defaultConnection = $connection;
+        $this->table             = $table;
+    }
+
+    /**
+     * Log data revisions in the db.
+     *
+     * @param  string  $action
+     * @param  string  $table
+     * @param  int     $id
+     * @param  array   $old
+     * @param  array   $new
+     * @param  string  $user
+     * @return void
+     */
+    public function revisionLog($action, $table, $id, array $old = [], array $new = [], $user = null,array $old_diff = [],array $new_diff = [])
+    {
+        $user = $this->parseUser($user);
+
+        $connection = $this->getCurrentConnection();
+
+        if(method_exists ($connection,'getDateFormat')) {
+            $format = $connection->getQueryGrammar()->getDateFormat();
+            $currentDatetime = (new DateTime)->format($format);
+        }else {
+            $currentDatetime = new \MongoDB\BSON\UTCDateTime(time() * 1000);
+        }
+
+        $connection->table($this->table)->insert([
+            'action'       => substr($action, 0, 255),
+            'table_name'   => substr($table, 0, 255),
+            'row_id'       => substr($id, 0, 255),
+            'old'          => $old_diff,
+            'new'          => $new_diff,
+            'user'         => substr($user, 0, 255),
+            'created_at'   => $currentDatetime,
+        ]);
+
+        $this->resetConnection();
+    }
+
+
+
+    /**
+     * Set custom connection for the next log.
+     *
+     * @param  \Illuminate\Database\ConnectionInterface $connection
+     * @return static
+     */
+    public function on(ConnectionInterface $connection)
+    {
+        $this->connection = $connection;
+
+        return $this;
+    }
+
+    /**
+     * Translate provided user to appropriate string.
+     *
+     * @param  mixed  $user
+     * @return string
+     */
+    protected function parseUser($user)
+    {
+        return (is_string($user) || is_numeric($user)) ? $user : ' -- ';
+    }
+
+    /**
+     * Return current connection instance to use for next log.
+     *
+     * @return \Illuminate\Database\ConnectionInterface
+     */
+    protected function getCurrentConnection()
+    {
+        return ($this->connection) ?: $this->defaultConnection;
+    }
+
+    /**
+     * Reset custom connection.
+     *
+     * @return void
+     */
+    protected function resetConnection()
+    {
+        $this->connection = null;
+    }
+
+    /**
+     * Get Server variable.
+     *
+     * @param  string $key
+     * @param  mixed  $default
+     * @return string|array
+     */
+    protected function getFromServer($key, $default = null)
+    {
+        return (isset($_SERVER[$key])) ? $_SERVER[$key] : $default;
+    }
+}
---
+++ src/Lumen/Listener.php
@@ -0,0 +1,138 @@
+<?php namespace Sofa\Revisionable\Lumen;
+
+use Sofa\Revisionable\Listener as ListenerInterface;
+use Sofa\Revisionable\UserProvider;
+use Sofa\Revisionable\Revisionable;
+use Dingo\Api\Routing\Helpers;
+
+class Listener implements ListenerInterface
+{
+    use Helpers;
+
+    /**
+     * User provider instance.
+     *
+     * @var mixed
+     */
+    protected $userProvider;
+
+    /**
+     * Create new listener.
+     *
+     * @param UserProvider $userProvider
+     */
+    public function __construct(UserProvider $userProvider)
+    {
+        $this->userProvider = $userProvider;
+    }
+
+    /**
+     * Handle created event.
+     *
+     * @param  \Sofa\Revisionable\Revisionable $revisioned
+     * @return null
+     */
+    public function onCreated(Revisionable $revisioned)
+    {
+        $this->log('created', $revisioned);
+    }
+
+    /**
+     * Handle updated event.
+     *
+     * @param  \Sofa\Revisionable\Revisionable $revisioned
+     * @return null
+     */
+    public function onUpdated(Revisionable $revisioned)
+    {
+        if (count($revisioned->getDiff())) {
+            $this->log('updated', $revisioned);
+        }
+    }
+
+    /**
+     * Handle deleted event.
+     *
+     * @param  \Sofa\Revisionable\Revisionable $revisioned
+     * @return null
+     */
+    public function onDeleted(Revisionable $revisioned)
+    {
+        $this->log('deleted', $revisioned);
+    }
+
+    /**
+     * Handle restored event.
+     *
+     * @param  \Sofa\Revisionable\Revisionable $revisioned
+     * @return null
+     */
+    public function onRestored(Revisionable $revisioned)
+    {
+        $this->log('restored', $revisioned);
+    }
+
+    /**
+     * Get currently logged in user.
+     *
+     * @return string|null
+     */
+    public function getCurrentUser()
+    {
+        $user='--';
+        if($this->userProvider->getUser()!=null){
+            $user =$this->userProvider->getUser();
+        }else {
+            $user = $this->auth()->user()->_id;
+        }
+
+        return $user;
+    }
+
+    /**
+     * Log the revision.
+     *
+     * @param  string $action
+     * @param  \Sofa\Revisionable\Revisionable $revisioned
+     * @return null
+     */
+    protected function log($action, Revisionable $revisioned)
+    {
+        if (!$revisioned->isRevisioned()) {
+            return;
+        }
+
+        $table = $revisioned->getTable();
+        $id    = $revisioned->getKey();
+        $user  = $this->getCurrentUser();
+        $old   = [];
+        $new   = [];
+        $old_diff   = [];
+        $new_diff   = [];
+
+        switch ($action) {
+            case 'created':
+                $new = $revisioned->getNewAttributes();
+                break;
+            case 'deleted':
+                $old = $revisioned->getOldAttributes();
+                break;
+            case 'updated':
+                $old = $revisioned->getOldAttributes();
+                $new = $revisioned->getNewAttributes();
+                $new_diff=$revisioned->getDiff();
+                foreach ($new_diff as $key => $nd){
+                    $old_diff[$key] = $old[$key];
+                }
+                break;
+        }
+
+        $logger = $revisioned->getRevisionableLogger();
+
+        if ($connection = $revisioned->getRevisionableConnection()) {
+            $logger->on($connection);
+        }
+
+        $logger->revisionLog($action, $table, $id, $old, $new, $user,$old_diff,$new_diff);
+    }
+}
